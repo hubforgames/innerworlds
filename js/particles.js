439a0877abc3abdc5fcd5c2e705f7d8faef65ccd
vinny <57708218+hubforgames@users.noreply.github.com>
2026-03-16 20:16:47 +0530

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -1,36 +1,45 @@
-const canvas=document.getElementById("stars")
-const ctx=canvas.getContext("2d")
+const canvas = document.getElementById("stars");
+const ctx = canvas.getContext("2d");
 
-function resize(){
-canvas.width=window.innerWidth
-canvas.height=window.innerHeight
-}
-resize()
-
-window.addEventListener("resize",resize)
-
-let stars=[]
-
-for(let i=0;i<120;i++){
-stars.push({
-x:Math.random()*canvas.width,
-y:Math.random()*canvas.height,
-size:Math.random()*2
-})
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 }
 
-function draw(){
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
 
-ctx.clearRect(0,0,canvas.width,canvas.height)
+let stars = [];
 
-ctx.fillStyle="white"
-
-stars.forEach(s=>{
-ctx.fillRect(s.x,s.y,s.size,s.size)
-})
-
-requestAnimationFrame(draw)
-
+for (let i = 0; i < 120; i++) {
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    size: Math.random() * 2,
+    speed: Math.random() * 0.3
+  });
 }
 
-draw()
+function drawStars() {
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "white";
+
+  stars.forEach(star => {
+
+    ctx.fillRect(star.x, star.y, star.size, star.size);
+
+    star.y += star.speed;
+
+    if (star.y > canvas.height) {
+      star.y = 0;
+      star.x = Math.random() * canvas.width;
+    }
+
+  });
+
+  requestAnimationFrame(drawStars);
+}
+
+drawStars();
