e318fe4df0cf56eb4e33c2262f9f28f52b180a29
vinny <57708218+hubforgames@users.noreply.github.com>
2026-03-16 21:05:08 +0530

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -11,13 +11,15 @@ window.addEventListener("resize", resizeCanvas);
 
 let stars=[];
 
-for(let i=0;i<140;i++){
+for(let i=0;i<120;i++){
 
 stars.push({
 x:Math.random()*canvas.width,
 y:Math.random()*canvas.height,
 size:Math.random()*2,
-speed:0.2 + Math.random()*0.4
+speed:Math.random()*0.15,
+opacity:Math.random(),
+twinkle:Math.random()*0.02
 });
 
 }
@@ -26,18 +28,24 @@ function animate(){
 
 ctx.clearRect(0,0,canvas.width,canvas.height);
 
-ctx.fillStyle="white";
-
 stars.forEach(star=>{
 
-ctx.fillRect(star.x,star.y,star.size,star.size);
-
 star.y += star.speed;
+
+star.opacity += star.twinkle;
+
+if(star.opacity > 1 || star.opacity < 0){
+star.twinkle *= -1;
+}
 
 if(star.y > canvas.height){
 star.y = 0;
 star.x = Math.random()*canvas.width;
 }
+
+ctx.fillStyle="rgba(255,255,255,"+star.opacity+")";
+
+ctx.fillRect(star.x,star.y,star.size,star.size);
 
 });
 
